75cdb424126d789d68767bf863b209002c16b2e3
Sean Huang <playpcfun@gmail.com>
2026-04-08 18:04:41 +0800

    商品功能 - 實作新增商品

--- src/main/java/com/sean/springbootmall/dao/ProductDao.java
+++ src/main/java/com/sean/springbootmall/dao/ProductDao.java
@@ -1,7 +1,10 @@
 package com.sean.springbootmall.dao;
 
+import com.sean.springbootmall.dto.ProductRequest;
 import com.sean.springbootmall.model.Product;
 
 public interface ProductDao {
     Product getProductById(Integer productId);
+
+    Integer createProduct(ProductRequest productRequest);
 }
--- src/main/java/com/sean/springbootmall/service/ProductService.java
+++ src/main/java/com/sean/springbootmall/service/ProductService.java
@@ -1,8 +1,11 @@
 package com.sean.springbootmall.service;
 
+import com.sean.springbootmall.dto.ProductRequest;
 import com.sean.springbootmall.model.Product;
 
 public interface ProductService {
 
     Product getProductById(Integer productId);
+
+    Integer createProduct(ProductRequest productRequest);
 }
