a135360bfe431e83cd2ea15084328d5fa172df08
Sean Huang <playpcfun@gmail.com>
2026-04-13 21:28:27 +0800

    帳號功能 - 登入

--- src/main/java/com/sean/springbootmall/service/UserService.java
+++ src/main/java/com/sean/springbootmall/service/UserService.java
@@ -1,5 +1,6 @@
 package com.sean.springbootmall.service;
 
+import com.sean.springbootmall.dto.UserLoginRequest;
 import com.sean.springbootmall.dto.UserRegisterRequest;
 import com.sean.springbootmall.model.User;
 
@@ -8,4 +9,6 @@ public interface UserService {
     User getUserById(Integer UserId);
 
     Integer register(UserRegisterRequest userRegisterRequest);
+
+    User login(UserLoginRequest userLoginRequest);
 }
